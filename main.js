3065271a0cbf0b082cd70d34745875e5c7384da8
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-21 20:41:19 +0530

    feat: async to fetch qr code

--- main.js
+++ main.js
@@ -7,4 +7,12 @@ let inputFieldValue = undefined;
 btn.addEventListener("click", (e) => {
   inputFieldValue = inputField.value;
   console.log(inputFieldValue);
+  generateQR(inputFieldValue);
 });
+
+async function generateQR(val) {
+  const url = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${val}`;
+  const res = await fetch(url);
+  const data = res.json();
+  console.log(data);
+}
