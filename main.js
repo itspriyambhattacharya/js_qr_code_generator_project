a746f086d2ae98ad7a5093853c2b2e64bd71caf6
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-21 22:13:58 +0530

    feat: added some checks while displaying the qr code

--- main.js
+++ main.js
@@ -8,6 +8,19 @@ let inputFieldValue = undefined;
 
 btn.addEventListener("click", (e) => {
   inputFieldValue = inputField.value;
-  imgTag.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputFieldValue}`;
+  if (inputFieldValue.length > 0) {
+    imgTag.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputFieldValue}`;
+    const fm = document.querySelector("#failed-msg");
+    fm.classList.add("d-none");
+    imgTag.classList.remove("d-none");
+  } else {
+    console.log("entering else");
+
+    imgTag.classList.add("d-none");
+    const elem = document.createElement("p");
+    elem.innerHTML = "Enter some text and try again";
+    elem.id = "failed-msg";
+    imgBox.appendChild(elem);
+  }
   imgBox.classList.remove("d-none");
 });
