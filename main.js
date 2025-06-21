0c233151e3d2ea5b6fd351fa30b582cc980636e8
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-21 21:58:52 +0530

    fix: removed unnecessary logs

--- main.js
+++ main.js
@@ -3,12 +3,11 @@ const btn = document.querySelector(`button[type="button"]`);
 const imgBox = document.querySelector("#img-box");
 const imgTag = document.querySelector("#qr-img");
 
-console.log(imgTag);
-
 console.log(btn);
 let inputFieldValue = undefined;
 
 btn.addEventListener("click", (e) => {
   inputFieldValue = inputField.value;
   imgTag.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputFieldValue}`;
+  imgBox.classList.remove("d-none");
 });
